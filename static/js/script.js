3443459ce751757d570ff7f41b115cf5008e3149
saswath266-code <saswath266@gmail.com>
2026-07-29 23:13:02 +0530

    Add auto expanding chat input

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,16 @@
-console.log("NovaChat Loaded");
+const textarea = document.querySelector("textarea");
+
+textarea.addEventListener("input", () => {
+    textarea.style.height = "auto";
+    textarea.style.height = textarea.scrollHeight + "px";
+});
+
+textarea.addEventListener("keydown", (event) => {
+
+    if (event.key === "Enter" && !event.shiftKey) {
+        event.preventDefault();
+
+        document.querySelector(".input-area button").click();
+    }
+
+});
